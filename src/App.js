95936037b16c85574e326980e97af4714edfc996
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-23 22:20:27 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from "./components/Header"
-import Meme from "./components/Meme"
+import Header from "./components/Header.js"
+import Meme from "./components/Meme.js"
  
 export default function App() {
     return (
